8e6666a3ed48d3dcc3398af55cf7df86e1dbeef2
Tlesis <jackwilliamson.1215@gmail.com>
2026-02-13 14:12:30 -0600

    fix silly, goofy formatting

--- src/eecs_388_lib.c
+++ src/eecs_388_lib.c
@@ -11,10 +11,8 @@
 /******************************************************************************
  *   Static functions
  *******************************************************************************/
-static void turnOffPWM(uint8_t timer)
-{
-	switch (timer)
-	{
+static void turnOffPWM(uint8_t timer) {
+	switch (timer) {
 		#if defined(TCCR1A) && defined(COM1A1)
 		case TIMER1A:   cbi(TCCR1A, COM1A1);    break;
 		#endif
@@ -24,15 +22,15 @@ static void turnOffPWM(uint8_t timer)
 		#if defined(TCCR1A) && defined(COM1C1)
 		case TIMER1C:   cbi(TCCR1A, COM1C1);    break;
 		#endif
-		
+
 		#if defined(TCCR2) && defined(COM21)
 		case  TIMER2:   cbi(TCCR2, COM21);      break;
 		#endif
-		
+
 		#if defined(TCCR0A) && defined(COM0A1)
 		case  TIMER0A:  cbi(TCCR0A, COM0A1);    break;
 		#endif
-		
+
 		#if defined(TCCR0A) && defined(COM0B1)
 		case  TIMER0B:  cbi(TCCR0A, COM0B1);    break;
 		#endif
@@ -42,7 +40,7 @@ static void turnOffPWM(uint8_t timer)
 		#if defined(TCCR2A) && defined(COM2B1)
 		case  TIMER2B:  cbi(TCCR2A, COM2B1);    break;
 		#endif
-		
+
 		#if defined(TCCR3A) && defined(COM3A1)
 		case  TIMER3A:  cbi(TCCR3A, COM3A1);    break;
 		#endif
@@ -55,17 +53,17 @@ static void turnOffPWM(uint8_t timer)
 
 		#if defined(TCCR4A) && defined(COM4A1)
 		case  TIMER4A:  cbi(TCCR4A, COM4A1);    break;
-		#endif					
+		#endif
 		#if defined(TCCR4A) && defined(COM4B1)
 		case  TIMER4B:  cbi(TCCR4A, COM4B1);    break;
 		#endif
 		#if defined(TCCR4A) && defined(COM4C1)
 		case  TIMER4C:  cbi(TCCR4A, COM4C1);    break;
-		#endif			
+		#endif
 		#if defined(TCCR4C) && defined(COM4D1)
 		case TIMER4D:	cbi(TCCR4C, COM4D1);	break;
-		#endif			
-			
+		#endif
+
 		#if defined(TCCR5A)
 		case  TIMER5A:  cbi(TCCR5A, COM5A1);    break;
 		case  TIMER5B:  cbi(TCCR5A, COM5B1);    break;
@@ -86,8 +84,7 @@ static void turnOffPWM(uint8_t timer)
  *          Delay execution of the next instruction by amount specified by
  * 			'ms'. Configured for 16MHz ATMega328P clock
  *******************************************************************************/
- void delay_ms(unsigned long ms)
-{
+ void delay_ms(unsigned long ms) {
 	uint32_t start = micros();
 
 	while (ms > 0) {
@@ -107,67 +104,64 @@ static void turnOffPWM(uint8_t timer)
  *          GPIO specified by 'pin' will be configured in the mode specified by
  * 			'mode' (either GPIO_OUTPUT or GPIO_INPUT)
  *******************************************************************************/
-void gpio_mode(uint8_t pin, uint8_t mode)
-{
-uint8_t bit = digitalPinToBitMask(pin);
-uint8_t port = digitalPinToPort(pin);
-volatile uint8_t *reg, *out;
+void gpio_mode(uint8_t pin, uint8_t mode) {
+	uint8_t bit = digitalPinToBitMask(pin);
+	uint8_t port = digitalPinToPort(pin);
+	volatile uint8_t *reg, *out;
 
-if (port == NOT_A_PIN) return;
+	if (port == NOT_A_PIN) return;
 
-reg = portModeRegister(port);
-out = portOutputRegister(port);
+	reg = portModeRegister(port);
+	out = portOutputRegister(port);
 
-if (mode == GPIO_INPUT) { 
-	uint8_t oldSREG = SREG;
-            cli();
-	*reg &= ~bit;
-	*out &= ~bit;
-	SREG = oldSREG;
-} else if (mode == INPUT_PULLUP) {
-	uint8_t oldSREG = SREG;
-            cli();
-	*reg &= ~bit;
-	*out |= bit;
-	SREG = oldSREG;
-} else {
-	uint8_t oldSREG = SREG;
-            cli();
-	*reg |= bit;
-	SREG = oldSREG;
-    }
-
-return;
-}
-
-void gpio_write(uint8_t pin, uint8_t val)
-{
-uint8_t timer = digitalPinToTimer(pin);
-uint8_t bit = digitalPinToBitMask(pin);
-uint8_t port = digitalPinToPort(pin);
-volatile uint8_t *out;
-
-if (port == NOT_A_PIN) return;
-// If the pin that support PWM output, we need to turn it off
-// before doing a digital write.
-if (timer != NOT_ON_TIMER) 
-	{
-	turnOffPWM(timer);
+	if (mode == GPIO_INPUT) { 
+		uint8_t oldSREG = SREG;
+		cli();
+		*reg &= ~bit;
+		*out &= ~bit;
+		SREG = oldSREG;
+	} else if (mode == INPUT_PULLUP) {
+		uint8_t oldSREG = SREG;
+		cli();
+		*reg &= ~bit;
+		*out |= bit;
+		SREG = oldSREG;
+	} else {
+		uint8_t oldSREG = SREG;
+		cli();
+		*reg |= bit;
+		SREG = oldSREG;
 	}
 
-out = portOutputRegister(port);
-
-uint8_t oldSREG = SREG;
-cli();
-
-if (val == OFF) {
-	*out &= ~bit;
-} else {
-	*out |= bit;
+	return;
 }
 
-SREG = oldSREG;
-return;
+void gpio_write(uint8_t pin, uint8_t val) {
+    uint8_t timer = digitalPinToTimer(pin);
+    uint8_t bit = digitalPinToBitMask(pin);
+    uint8_t port = digitalPinToPort(pin);
+    volatile uint8_t *out;
+
+    if (port == NOT_A_PIN) return;
+    // If the pin that support PWM output, we need to turn it off
+    // before doing a digital write.
+    if (timer != NOT_ON_TIMER) {
+        turnOffPWM(timer);
+    }
+
+    out = portOutputRegister(port);
+
+    uint8_t oldSREG = SREG;
+    cli();
+
+    if (val == OFF) {
+        *out &= ~bit;
+    } else {
+        *out |= bit;
+    }
+
+    SREG = oldSREG;
+    return;
 }
 
 void uart_init() {
@@ -211,16 +205,12 @@ void ser_printline(const char *str) {
 }
 
 char ser_read() {
-    while (!(UCSR0A & (1 << RXC0))) {
-        // Wait until data is received
-    }
+    while (!(UCSR0A & (1 << RXC0))); // Wait until data is received
     return UDR0; // Return received byte from buffer
 }
 
 void ser_write(const char c) {
-    while (!(UCSR0A & (1 << UDRE0))) {
-        // Wait until transmit buffer is empty
-    }
+    while (!(UCSR0A & (1 << UDRE0))) // Wait until transmit buffer is empty
     UDR0 = c; // Load data into transmit register
 }
 
--- src/eecs_388_tfmini.cpp
+++ src/eecs_388_tfmini.cpp
@@ -15,14 +15,11 @@
  *          Runs initialization calls one time on power up
  *          Input/Output (IO) pins are configured
  *******************************************************************************/
-void setup() 
-{
-gpio_mode(GPIO_13, GPIO_OUTPUT);
-gpio_mode(GPIO_12, GPIO_OUTPUT);
+void setup() {
+    gpio_mode(GPIO_13, GPIO_OUTPUT);
+    gpio_mode(GPIO_12, GPIO_OUTPUT);
 
-uart_init();
-
-return;
+    uart_init();
 }
 
 /******************************************************************************
@@ -33,41 +30,36 @@ return;
  *          Performs a single iteration of the system's function
  *          Repeates indefinetely unless the board is reset or powered off
  *******************************************************************************/
-void loop() 
-{
-uint16_t dist = 0;              /* LIDAR distance data is 16 bits. */
+void loop()  {
+    uint16_t dist = 0;              /* LIDAR distance data is 16 bits. */
 
-ser_printline("Setup completed.");
-ser_write( '\n' );
+    ser_printline("Setup completed.");
+    ser_write( '\n' );
 
-while( 1 )
-    { 
-    /* 
-        Task 1.2: 
-        - read a data frame from the TFmini sensor
-        
-        - from Table 6, we know each LIDAR measurement sends 9 bytes
-        - the function `ser_read()` can process 1 byte per function call
+    while( 1 ) {
+        /*
+         *  Task 1.2:
+         *  - read a data frame from the TFmini sensor
 
-        - This if-statement below will read (and check) the first two bytes of a valid data frame
-        - You will need to write code to read the subsequent bytes to get the distance data
-        - You will need to process the data received from the sensor to get the correct distance value
-    */
-    if ('Y' == ser_read() && 'Y' == ser_read()) 
-        {
-        /* 
-            Task 1.3: 
-            - turn on the red LED if the distance measured is less than 50 cm. 
-            - otherwise turn on the green LED 
+         *  - from Table 6, we know each LIDAR measurement sends 9 bytes
+         *  - the function `ser_read()` can process 1 byte per function call
 
-            - print the measured value to the console (i.e. the serial monitor) 
-            - You can use ser_printf() or ser_printline() or ser_write()
-            
-            - Helpful guide for printing with variadic function similiar to printf()
-            - https://en.cppreference.com/w/cpp/io/c/fprintf
-        */
+         *  - This if-statement below will read (and check) the first two bytes of a valid data frame
+         *  - You will need to write code to read the subsequent bytes to get the distance data
+         *  - You will need to process the data received from the sensor to get the correct distance value
+         */
+        if ('Y' == ser_read() && 'Y' == ser_read()) {
+            /*
+             * Task 1.3:
+             *  - turn on the red LED if the distance measured is less than 50 cm. 
+             *  - otherwise turn on the green LED 
+             *
+             *  - print the measured value to the console (i.e. the serial monitor) 
+             *  - You can use ser_printf() or ser_printline() or ser_write()
+             *
+             *  - Helpful guide for printing with variadic function similiar to printf()
+             *  - https://en.cppreference.com/w/cpp/io/c/fprintf
+             */
         }
     }
-
-return;
 }
